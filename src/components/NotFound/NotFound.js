70de9fcc22d246c8f73a2de09b6b45a9678ff927
Abdullah Al Mamun <abdullahmamun1597@gmail.com>
2022-04-04 17:15:31 +0600

    dashboard add

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,8 +3,8 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-            <h1 className='text-center'>You are trying to access the page is not available!!!</h1>
-            <p>404</p>
+            <h1 className='text-center text-2xl mt-10'>You are trying to access the page is not available!!!</h1>
+            <p className='text-center text-5xl text-red-600'>404</p>
         </div>
     );
 };
